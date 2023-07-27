4a6390e3c7d88ef948b27d78fdbe7979f325393b
knskxx13 <138800047+knskxx13@users.noreply.github.com>
2023-07-28 01:22:23 +0900

    Add files via upload

--- api_list.py
+++ api_list.py
@@ -1,1 +1,2 @@
-API_LIST=["http://127.0.0.1:8002","http://127.0.0.1:8003"]
+DEV_API_LIST=["http://127.0.0.1:8002","http://127.0.0.1:8003"]
+PRD_API_LIST=["https://blockchain-api-2-ohp6.onrender.com","https://blockchain-api-3-te3o.onrender.com"]
